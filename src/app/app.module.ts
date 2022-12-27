32a9ca97ab8a8bf11ac2268fc59273e39714df21
monacho <monacho_4@hotmail.com>
2022-12-27 17:04:58 -0500

    cv

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,12 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { CertificadoComponent } from './certificado/certificado.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    CertificadoComponent
   ],
   imports: [
     BrowserModule
